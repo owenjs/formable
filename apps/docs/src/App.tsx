d4bfcc06034dcfef0ff1368570ea130e016d51c7
owenjs <owen.evans115@gmail.com>
2023-02-23 11:17:58 +0000

    husky test

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { Button } from "ui";
+import reactLogo from "./assets/react.svg"
+import { Button } from "ui"
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="App Apps">
       <div>
+
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
