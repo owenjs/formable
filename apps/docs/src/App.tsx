b5061a4c715dda6b7f9247023c5b906bf17ca877
owenjs <owen.evans115@gmail.com>
2023-02-23 11:19:27 +0000

    husky test

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App Apps">
+    <div className="App Appsss">
       <div>
 
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
